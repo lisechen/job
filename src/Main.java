4f821e7cc176cbb9e2a0204cea20883fe48b6ab2
lisechen <1258073422@qq.com>
2020-06-26 21:08:07 +0800

    第二次提交

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         //Ticket01 t = new Ticket01();//synchronized锁线程任务对象。
         Ticket t = new Ticket();//没有用锁。
-        //创建4个线程同时卖票
         //Ticket02 t = new Ticket02();//ReentrantLock锁线程任务对象。
+        //创建4个线程同时卖票
         Thread t1 = new Thread(t);
         Thread t2 = new Thread(t);
         Thread t3 = new Thread(t);
